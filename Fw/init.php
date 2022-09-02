6b981daaa08f6dcdd220586a3820db2e5856183f
Natalia Panfilenka <choco-cat@mail.u>
2022-09-02 10:19:54 +0300

    feat: autoload function

--- Fw/init.php
+++ Fw/init.php
@@ -3,3 +3,12 @@
  * autoload function
  * session start
  */
+
+spl_autoload_register(function($class_name) {
+    $file =  dirname(realpath(__FILE__)) . '/' . str_replace('\\', '/', $class_name) . '.php';
+    if (file_exists($file)) {
+       include $file;
+    }
+});
+
+session_start();
