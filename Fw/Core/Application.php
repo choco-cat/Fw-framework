000dd97942eb89bad4d5121d52952a8dad74409c
Natalia Panfilenka <choco-cat@mail.u>
2022-09-05 15:00:40 +0300

    feat: output buffer

--- Fw/Core/Application.php
+++ Fw/Core/Application.php
@@ -24,13 +24,32 @@ final class Application
         self::$template = Template::getInstance();
     }
 
+    public function startBuffer()
+    {
+        ob_start();
+    }
+
+    public function endBuffer()
+    {
+        $output = ob_get_contents();
+        ob_end_clean();
+        echo $output;
+    }
+
+    public function restartBuffer()
+    {
+        ob_clean();
+    }
+
     public function header()
     {
+        self::startBuffer();
         echo self::$template->getHeader();
     }
 
     public function footer()
     {
         echo self::$template->getFooter();
+        self::endBuffer();
     }
 }
