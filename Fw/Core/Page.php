2f6a0c69dea9d30a69ebce3e04687b0519cffa15
Natalia Panfilenka <choco-cat@mail.u>
2022-09-06 10:49:07 +0300

    feat: structure of class Page

---
+++ Fw/Core/Page.php
@@ -0,0 +1,50 @@
+<?php
+
+if (!defined('IN_FW')) {
+    exit;
+}
+
+class Page
+{
+    private $properties = [];
+
+    public function addJs($src)
+    {
+
+    }
+
+    public function addCss($src)
+    {
+
+    }
+
+    public function addString($src)
+    {
+
+    }
+
+    public function setProperty($src)
+    {
+
+    }
+
+    public function getProperty($src)
+    {
+
+    }
+
+    public function showProperty($src)
+    {
+
+    }
+
+    public function getAllReplace($src)
+    {
+
+    }
+
+    public function showHead($src)
+    {
+
+    }
+}
