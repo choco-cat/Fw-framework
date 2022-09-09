f691698292fd315db06865373979bf87ba7cfd39
Natalia Panfilenka <choco-cat@mail.u>
2022-09-09 13:00:36 +0300

    feat: create Template and Base classes

---
+++ fw/core/component/Base.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Fw\Core\Component;
+
+if (!defined('IN_FW')) {
+    exit;
+}
+
+abstract class Base
+{
+    public $result = array();
+    public $id;
+    public $params;
+    public $template;
+    public $__path;
+
+    abstract public function executeComponent();
+
+    public function __construct()
+    {
+    }
+}
---
+++ fw/core/component/Template.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Fw\Core\Component;
+
+if (!defined('IN_FW')) {
+    exit;
+}
+
+class Template
+{
+    public $id;
+    public $__relativePath;
+    public $__path;
+
+    public function __construct()
+    {
+    }
+
+    public function render($page = 'template')
+    {
+    }
+}
